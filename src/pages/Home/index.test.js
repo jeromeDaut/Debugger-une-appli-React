3941c71811ee4edb02250f41b8d5c53b35b20be0
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-04-24 11:07:19 +0200

    suppression async sur un test

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,9 +1,8 @@
 import { act, fireEvent, render, screen, within } from "@testing-library/react";
 import Home from "./index";
+import EventList from "../../containers/Events";
 
-// jest.mock("../../contexts/DataContext", () => ({
-//   useData: jest.fn(() => ({ data: { events: [] } })),
-// }));
+
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
     render(<Home />);
@@ -12,7 +11,7 @@ describe("When Form is created", () => {
     await screen.findByText("Prénom");
     await screen.findByText("Personel / Entreprise");
   });
-  
+
   describe("and a click is triggered on the submit button", () => {
     it("the success message is displayed", async () => {
       render(<Home />);
@@ -22,21 +21,20 @@ describe("When Form is created", () => {
           cancelable: true,
           bubbles: true,
         })
-        );
-        await screen.findByText("En cours");
-        act(()=>jest.advanceTimersByTime(1000))
-        await screen.findByText("Envoyer");
-      });
+      );
+      await screen.findByText("En cours");
+      act(()=>jest.advanceTimersByTime(1000))
+      await screen.findByText("Envoyer");
     });
-    
   });
   
-  
+});
 
-  describe("When a page is created", () => {
-    it("a list of events is displayed", () => {
-      
-    });
+describe("When a page is created", () => {
+  it("a list of events is displayed", () => {
+    const { getAllByAltText } = render(<EventList />);
+    const images = getAllByAltText(/cover/i);
+    expect(images.length).toBe(9);
   });
   
   it("a list a people is displayed", async () => {
@@ -47,14 +45,13 @@ describe("When Form is created", () => {
     const figures = within(title.closest("section")).getAllByRole("img");
     expect(figures).toHaveLength(6);
   });
-  
-  it("a footer is displayed", async () => {
+
+  it("a footer is displayed", () => {
     render(<Home />);
     // Trouver l'élément avec le rôle "contentinfo" pour le footer
     expect(screen.getByRole("contentinfo")).toBeInTheDocument();
   });
-  
-  it("an event card, with the last event, is displayed",  () => {
-    
+
+  it("an event card, with the last event, is displayed", async () => {
   });
-  // });
+});
