10d30dcc7c9326193772b47b1ab42f1564c3f7e7
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-04-24 10:25:58 +0200

    test en cours

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,6 +1,9 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen, within } from "@testing-library/react";
 import Home from "./index";
 
+// jest.mock("../../contexts/DataContext", () => ({
+//   useData: jest.fn(() => ({ data: { events: [] } })),
+// }));
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
     render(<Home />);
@@ -9,7 +12,7 @@ describe("When Form is created", () => {
     await screen.findByText("Prénom");
     await screen.findByText("Personel / Entreprise");
   });
-
+  
   describe("and a click is triggered on the submit button", () => {
     it("the success message is displayed", async () => {
       render(<Home />);
@@ -19,26 +22,39 @@ describe("When Form is created", () => {
           cancelable: true,
           bubbles: true,
         })
-      );
-      await screen.findByText("En cours");
-      await screen.findByText("Message envoyé !");
+        );
+        await screen.findByText("En cours");
+        act(()=>jest.advanceTimersByTime(1000))
+        await screen.findByText("Envoyer");
+      });
+    });
+    
+  });
+  
+  
+
+  describe("When a page is created", () => {
+    it("a list of events is displayed", () => {
+      
     });
   });
-
-});
-
-
-describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
-    // to implement
-  })
-  it("a list a people is displayed", () => {
-    // to implement
-  })
-  it("a footer is displayed", () => {
-    // to implement
-  })
-  it("an event card, with the last event, is displayed", () => {
-    // to implement
-  })
-});
+  
+  it("a list a people is displayed", async () => {
+    render(<Home />);
+    // Trouver le titre pour la section "Notre équipe"
+    const title = await screen.findByRole("heading", { name: "Notre équipe" });
+    // Trouver tous les éléments avec le rôle "img" à l'intérieur de la section "Notre équipe"
+    const figures = within(title.closest("section")).getAllByRole("img");
+    expect(figures).toHaveLength(6);
+  });
+  
+  it("a footer is displayed", async () => {
+    render(<Home />);
+    // Trouver l'élément avec le rôle "contentinfo" pour le footer
+    expect(screen.getByRole("contentinfo")).toBeInTheDocument();
+  });
+  
+  it("an event card, with the last event, is displayed",  () => {
+    
+  });
+  // });
